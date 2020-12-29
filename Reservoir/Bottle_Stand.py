ebcc37b9aef4b33174fea6d7448c69d0ac7bcc2a
Josh Ward <ward.joshua92@yahoo.com>
2020-12-29 06:39:21 -0800

    create bottle tripod stand for siphon based reservoir

--- Reservoir/Bottle_Stand.py
+++ Reservoir/Bottle_Stand.py
@@ -1,20 +1,25 @@
-# TODO: Stand for holding a 2-liter type bottle upside down next to Bucket
 # TODO: consider a brita filter as a mount since corrosion seems negligable
 #       as long as chlorine bumps off and minerals/metals are filtered.
 #       open loop will corrode a bit anyways but shouldnt change conductive
 #       property of aluminum.
-# TODO: this isnt unreasonable given 100% infill with large wall_thickness.
+#       this isnt unreasonable given 100% infill with large wall_thickness.
 #       can just insert brita filter in a 3D printed stand.
 #       can use arduino for filter life measurement using the flap button
 #       method of brita
-# TODO: make legs with flex hinges so they can be printed seperately.
+# TODO: consider an RGB fade color for water height when arduino
+#       circuit is developed.
 from solid import *
 from solid.utils import *
+from solid import screw_thread
+from math import sin, cos, tan
 import toml
 import os
 
+# This is currently the highest priority print.
 
-def bottle_stand(leg_height, leg_camber, wall_thickness, bottle_diameter, bottle_depth):
+
+def bottle_stand(leg_height, leg_camber, wall_thickness, nozzle_diameter, nozzle_depth,
+                 bottle_diameter, bottle_depth):
     '''
     PARAMETERS:
         leg_height: the height of the riser legs
@@ -23,29 +28,54 @@ def bottle_stand(leg_height, leg_camber, wall_thickness, bottle_diameter, bottle
         bottle_diameter: the diameter of the bottle body past the nozzle
         bottle_depth: the length of the bottle nozzle until the bottle body
     '''
-    # create tripod base
-    # TODO: do the trig on right offset given camber
-    leg = rotate([0, -leg_camber, 0])(translate([bottle_diameter/2, 0, leg_height/2])(
-        cylinder(r=wall_thickness, h=leg_height, center=True)))
-    leg += rotate(120)(leg)
-    leg += rotate(240)(leg)
-
-    # a synthetic "floor" for ensuring legs are level through subtraction.
-    base_plate = cube([2*leg_height, 2*leg_height,
-                       2*wall_thickness], center=True)
-    leg = leg - hole()(base_plate)
-
+    # create bottle holder
     orifice_solid = cylinder(r=bottle_diameter/2 + wall_thickness,
                              h=bottle_depth, center=True)
-    orifice = orifice_solid - hole()(cylinder(r=bottle_diameter /
-                                              2, h=bottle_depth, center=True))
+    # hole here since legs may jut
+    orifice = orifice_solid - cylinder(r=bottle_diameter /
+                                       2, h=bottle_depth, center=True)
+    # raise orifice to leg stand height
     orifice = up(leg_height+bottle_depth/2)(orifice)
 
     # the cusp of the nozzle holder
-    throat_solid = cylinder(r1=,r2=,h=,center=True)
+    throat_solid = cylinder(r1=nozzle_diameter/2 + wall_thickness,
+                            r2=bottle_diameter/2 + wall_thickness, h=nozzle_depth, center=True)
+    # hole here since legs may jut
+    throat = throat_solid - cylinder(r1=nozzle_diameter/2,
+                                     r2=bottle_diameter/2, h=nozzle_depth, center=True)
+    throat = up(leg_height-nozzle_depth/2)(throat)
 
-    stand = leg + orifice
-    return stand
+    # create tripod base
+    # a synthetic "floor" for ensuring legs are level through subtraction.
+    base_plate = cube([2*leg_height+bottle_diameter, 2*leg_height+bottle_diameter,
+                       2*wall_thickness], center=True)
+    rotation_offset_x = leg_height*sin(radians(leg_camber))
+    rotation_offset_y = leg_height*cos(radians(leg_camber))
+    # solve the intersection of the leg with the orifice penetrating wall_thickness
+    screw_insert_distance = 2*wall_thickness*tan(radians(leg_camber))
+    leg = cylinder(r=wall_thickness, h=leg_height +
+                   screw_insert_distance, center=True)
+    # TODO: adjust screw params for precision of print, possibly extract parameter
+    section = screw_thread.default_thread_section(
+        tooth_height=10, tooth_depth=5)
+    leg_screw = screw_thread.thread(outline_pts=section,
+                                    inner_rad=wall_thickness,
+                                    pitch=screw_insert_distance,
+                                    length=screw_insert_distance,
+                                    segments_per_rot=50,
+                                    neck_in_degrees=90,
+                                    neck_out_degrees=90)
+    leg = leg+up(leg_height/2)(leg_screw)
+    leg = rotate([0, -leg_camber, 0])(up(leg_height /
+                                         2+screw_insert_distance/2)(leg))
+    leg = leg - hole()(base_plate)
+    legs = right(rotation_offset_x + bottle_diameter/2)(leg)
+    legs += rotate(120)(legs)
+    legs += rotate(240)(legs)
+
+    stand = orifice + throat
+    stand = stand - legs
+    return stand, leg
 
 
 def render_object(render_object, filename):
@@ -64,5 +94,6 @@ def render_object(render_object, filename):
 
 if __name__ == '__main__':
     config = toml.load("configuration.toml")
-    stand = bottle_stand(**config)
+    stand, leg = bottle_stand(**config)
     render_object(stand, "bottle_stand")
+    render_object(leg, "x3_bottle_stand_leg")
